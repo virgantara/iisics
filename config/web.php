6ca12ba69f5618c0dc037ac5bffb6717b489753b
virgantara <oddy@unida.gontor.ac.id>
2022-11-20 08:39:00 +0700

    fi

--- config/web.php
+++ config/web.php
@@ -59,14 +59,7 @@ $config = [
             // You have to set 'useFileTransport' to false and configure a transport for the mailer to send real emails.
             'useFileTransport' => false,
 
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'noreply@unida.gontor.ac.id',
-                'password' => 'cKfhYdaN',
-                'port' => '587',
-                'encryption' => 'tls',                                  
-            ], 
+            'transport' => $params['email'] 
 
         ],
         'view' => [
